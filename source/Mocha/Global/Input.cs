990fd66e3c7bf1edf283f0695a351a663d57da3b
Alex <alex@gu3.me>
2022-04-11 13:59:59 +0100

    Fix some input stuff

--- source/Mocha/Global/Input.cs
+++ source/Mocha/Global/Input.cs
@@ -22,125 +22,123 @@ public static partial class Input
 	{
 		var io = ImGui.GetIO();
 
-		if ( io.WantCaptureMouse )
+		if ( Sdl2Native.SDL_SetRelativeMouseMode( !Editor.Instance.ShouldRender ) != 0 )
+			throw new Exception();
+
+		var lastKeysDown = InputSnapshot?.KeysDown.ToList() ?? new();
+		var lastKeyEvents = InputSnapshot?.KeyEvents.ToList() ?? new();
+		var lastMouseEvents = InputSnapshot?.MouseEvents.ToList() ?? new();
+		InputSnapshot ??= new();
+		InputSnapshot.KeysDown = lastKeysDown.ToList();
+		InputSnapshot.LastKeysDown = lastKeysDown;
+		InputSnapshot.MouseDelta = Vector2.Zero;
+
+		Sdl2Native.SDL_PumpEvents();
+
+		List<Veldrid.KeyEvent> veldridKeyEvents = new( lastKeyEvents );
+		List<Veldrid.MouseEvent> veldridMouseEvents = new( lastMouseEvents );
+		List<char> keyCharPresses = new();
+
+		SDL_Event e;
+		while ( Sdl2Native.SDL_PollEvent( &e ) != 0 )
 		{
-			Sdl2Native.SDL_SetRelativeMouseMode( false );
+			switch ( e.type )
+			{
+				case SDL_EventType.MouseMotion:
+					SDL_MouseMotionEvent mme = Unsafe.Read<SDL_MouseMotionEvent>( &e );
+					InputSnapshot.MouseDelta = new( mme.xrel, mme.yrel );
+					InputSnapshot.MousePosition = new( mme.x, mme.y );
+
+					break;
+				case SDL_EventType.MouseButtonDown:
+				case SDL_EventType.MouseButtonUp:
+					SDL_MouseButtonEvent mbe = Unsafe.Read<SDL_MouseButtonEvent>( &e );
+					bool isButtonDown = (mbe.type == SDL_EventType.MouseButtonDown);
+					var veldridButton = Veldrid.MouseButton.Left;
+
+					switch ( mbe.button )
+					{
+						case SDL_MouseButton.Left:
+							veldridButton = Veldrid.MouseButton.Left;
+							InputSnapshot.MouseLeft = isButtonDown;
+							break;
+						case SDL_MouseButton.Right:
+							veldridButton = Veldrid.MouseButton.Right;
+							InputSnapshot.MouseRight = isButtonDown;
+							break;
+						default:
+							break;
+					}
+
+					var mbeVeldrid = new Veldrid.MouseEvent( veldridButton, isButtonDown );
+
+					veldridMouseEvents.RemoveAll( x => x.MouseButton == veldridButton );
+					veldridMouseEvents.Add( mbeVeldrid );
+					break;
+				case SDL_EventType.KeyDown:
+				case SDL_EventType.KeyUp:
+
+					SDL_KeyboardEvent kbe = Unsafe.Read<SDL_KeyboardEvent>( &e );
+					bool isKeyDown = (kbe.type == SDL_EventType.KeyDown);
+
+					if ( isKeyDown )
+					{
+						if ( !InputSnapshot.KeysDown.Any( x => x.sym == kbe.keysym.sym ) )
+							InputSnapshot.KeysDown.Add( kbe.keysym );
+					}
+					else
+					{
+						InputSnapshot.KeysDown.RemoveAll( x => x.sym == kbe.keysym.sym );
+					}
+
+					var veldridModifiers = Veldrid.ModifierKeys.None;
+					var veldridKey = MapKey( kbe.keysym );
+					var kbeVeldrid = new Veldrid.KeyEvent( veldridKey, isKeyDown, veldridModifiers );
+
+					veldridKeyEvents.Add( kbeVeldrid );
+
+					break;
+
+				case SDL_EventType.WindowEvent:
+					SDL_WindowEvent we = Unsafe.Read<SDL_WindowEvent>( &e );
+					switch ( we.@event )
+					{
+						case SDL_WindowEventID.Resized:
+						case SDL_WindowEventID.SizeChanged:
+							var newSize = new Point2( we.data1, we.data2 );
+							Screen.UpdateFrom( newSize ); // TODO: Can we hook this up to Event.Window.Resized?
+							Event.Run( Event.Window.ResizedAttribute.Name, newSize );
+							break;
+						default:
+							break;
+					}
+
+					break;
+			}
 		}
-		else
+
+		InputSnapshot.Forward = 0;
+		InputSnapshot.Left = 0;
+		InputSnapshot.Up = 0;
+
+		if ( IsKeyPressed( SDL_Keycode.SDLK_a ) )
+			InputSnapshot.Left -= 1;
+		if ( IsKeyPressed( SDL_Keycode.SDLK_d ) )
+			InputSnapshot.Left += 1;
+		if ( IsKeyPressed( SDL_Keycode.SDLK_w ) )
+			InputSnapshot.Forward += 1;
+		if ( IsKeyPressed( SDL_Keycode.SDLK_s ) )
+			InputSnapshot.Forward -= 1;
+
+		InputSnapshot.KeyEvents = veldridKeyEvents;
+		InputSnapshot.MouseEvents = veldridMouseEvents;
+		InputSnapshot.KeyCharPresses = keyCharPresses;
+
+		if ( Editor.Instance.ShouldRender )
 		{
-			var lastKeysDown = InputSnapshot?.KeysDown.ToList() ?? new();
-			InputSnapshot = new();
-			InputSnapshot.KeysDown = lastKeysDown;
-			InputSnapshot.LastKeysDown = lastKeysDown;
-
-			Sdl2Native.SDL_SetRelativeMouseMode( true );
-			Sdl2Native.SDL_PumpEvents();
-
-			List<Veldrid.KeyEvent> veldridKeyEvents = new();
-			List<Veldrid.MouseEvent> veldridMouseEvents = new();
-			List<char> keyCharPresses = new();
-
-			SDL_Event e;
-			while ( Sdl2Native.SDL_PollEvent( &e ) != 0 )
-			{
-				switch ( e.type )
-				{
-					case SDL_EventType.MouseMotion:
-						SDL_MouseMotionEvent mme = Unsafe.Read<SDL_MouseMotionEvent>( &e );
-						InputSnapshot.MouseDelta = new( mme.xrel, mme.yrel );
-						InputSnapshot.MousePosition = new( mme.x, mme.y );
-
-						break;
-					case SDL_EventType.MouseButtonDown:
-					case SDL_EventType.MouseButtonUp:
-						SDL_MouseButtonEvent mbe = Unsafe.Read<SDL_MouseButtonEvent>( &e );
-						bool isButtonDown = (mbe.type == SDL_EventType.MouseButtonDown);
-						var veldridButton = Veldrid.MouseButton.Left;
-
-						switch ( mbe.button )
-						{
-							case SDL_MouseButton.Left:
-								veldridButton = Veldrid.MouseButton.Left;
-								InputSnapshot.MouseLeft = isButtonDown;
-								break;
-							case SDL_MouseButton.Right:
-								veldridButton = Veldrid.MouseButton.Right;
-								InputSnapshot.MouseRight = isButtonDown;
-								break;
-							default:
-								break;
-						}
-
-						var mbeVeldrid = new Veldrid.MouseEvent( veldridButton, isButtonDown );
-
-						veldridMouseEvents.Add( mbeVeldrid );
-						break;
-					case SDL_EventType.KeyDown:
-					case SDL_EventType.KeyUp:
-
-						SDL_KeyboardEvent kbe = Unsafe.Read<SDL_KeyboardEvent>( &e );
-						bool isKeyDown = (kbe.type == SDL_EventType.KeyDown);
-
-						if ( isKeyDown )
-						{
-							if ( !InputSnapshot.KeysDown.Any( x => x.sym == kbe.keysym.sym ) )
-								InputSnapshot.KeysDown.Add( kbe.keysym );
-						}
-						else
-						{
-							InputSnapshot.KeysDown.RemoveAll( x => x.sym == kbe.keysym.sym );
-						}
-
-						var veldridModifiers = Veldrid.ModifierKeys.None;
-						var veldridKey = MapKey( kbe.keysym );
-						var kbeVeldrid = new Veldrid.KeyEvent( veldridKey, isKeyDown, veldridModifiers );
-
-						veldridKeyEvents.Add( kbeVeldrid );
-
-						break;
-
-					case SDL_EventType.WindowEvent:
-						SDL_WindowEvent we = Unsafe.Read<SDL_WindowEvent>( &e );
-						switch ( we.@event )
-						{
-							case SDL_WindowEventID.Resized:
-							case SDL_WindowEventID.SizeChanged:
-								var newSize = new Point2( we.data1, we.data2 );
-								Screen.UpdateFrom( newSize ); // TODO: Can we hook this up to Event.Window.Resized?
-								Event.Run( Event.Window.ResizedAttribute.Name, newSize );
-								break;
-							default:
-								break;
-						}
-
-						break;
-				}
-			}
-
-			InputSnapshot.Forward = 0;
-			InputSnapshot.Left = 0;
-			InputSnapshot.Up = 0;
-
-			if ( IsKeyPressed( SDL_Keycode.SDLK_a ) )
-				InputSnapshot.Left -= 1;
-			if ( IsKeyPressed( SDL_Keycode.SDLK_d ) )
-				InputSnapshot.Left += 1;
-			if ( IsKeyPressed( SDL_Keycode.SDLK_w ) )
-				InputSnapshot.Forward += 1;
-			if ( IsKeyPressed( SDL_Keycode.SDLK_s ) )
-				InputSnapshot.Forward -= 1;
-
-			InputSnapshot.KeyEvents = veldridKeyEvents;
-			InputSnapshot.MouseEvents = veldridMouseEvents;
-			InputSnapshot.KeyCharPresses = keyCharPresses;
-
-			Log.Trace( $"{lastKeysDown.Count} vs {InputSnapshot.KeysDown.Count}" );
-
-			var newKeys = InputSnapshot.KeysDown.Where( x => !lastKeysDown.Contains( x ) ).ToArray();
-			foreach ( var key in newKeys )
-			{
-				Log.Trace( $"Key {key} was pressed" );
-			}
+			InputSnapshot.MouseDelta = Vector2.Zero;
+			InputSnapshot.MouseLeft = false;
+			InputSnapshot.MouseRight = false;
 		}
 	}
 
