14a27cf249da5d04ea977b1359eb5ae7d508040b
Alex <alex@gu3.me>
2022-06-13 02:15:36 +0100

    Simple hang detector

---
+++ source/Mocha.HangDetector/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Mocha.HangDetector
+{
+	partial class MainForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && (components != null) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( MainForm ) );
+			this.label1 = new System.Windows.Forms.Label();
+			this.HangDetectionWorker = new System.ComponentModel.BackgroundWorker();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font( "Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point );
+			this.label1.Location = new System.Drawing.Point( 12, 9 );
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size( 276, 25 );
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Hang Detected...";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// HangDetectionWorker
+			// 
+			this.HangDetectionWorker.DoWork += new System.ComponentModel.DoWorkEventHandler( this.HangDetectionWorker_DoWork );
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF( 7F, 15F );
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject( "$this.BackgroundImage" )));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size( 300, 150 );
+			this.ControlBox = false;
+			this.Controls.Add( this.label1 );
+			this.DoubleBuffered = true;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Hang Detected";
+			this.Load += new System.EventHandler( this.MainForm_Load );
+			this.ResumeLayout( false );
+
+		}
+
+		#endregion
+
+		private Label label1;
+		private System.ComponentModel.BackgroundWorker HangDetectionWorker;
+	}
+}
