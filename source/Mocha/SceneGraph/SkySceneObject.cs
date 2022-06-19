219e01d34facdc8fbdf67ba1ceea66001d2a365c
Alex <alex@gu3.me>
2022-06-19 01:33:38 +0100

    Reimplement skybox

---
+++ source/Mocha/SceneGraph/SkySceneObject.cs
@@ -0,0 +1,35 @@
+﻿namespace Mocha.Renderer;
+
+public class SkySceneObject : ModelSceneObject
+{
+	public float SunIntensity { get; set; } = 32.0f;
+	public float PlanetRadius { get; set; } = 6371000;
+	public float AtmosphereRadius { get; set; } = 6381000;
+
+	public SkySceneObject( IEntity entity ) : base( entity )
+	{
+	}
+
+	public override void Render( CommandList commandList )
+	{
+		var currentCamera = SceneWorld.Current.Camera;
+
+		var uniformBuffer = new SkyUniformBuffer
+		{
+			g_mModel = ModelMatrix,
+			g_mView = SceneWorld.Current.Camera.ViewMatrix,
+			g_mProj = SceneWorld.Current.Camera.ProjMatrix,
+			g_vLightPos = SceneWorld.Current.Sun.Transform.Position,
+			g_flTime = Time.Now,
+			g_vLightColor = SceneWorld.Current.Sun.Color.ToVector4(),
+			g_vCameraPos = SceneWorld.Current.Camera.Transform.Position,
+
+			g_flPlanetRadius = PlanetRadius,
+			g_flAtmosphereRadius = AtmosphereRadius,
+			g_flSunIntensity = SunIntensity,
+			g_vSunPos = SceneWorld.Current.Sun.Transform.Rotation.Backward
+		};
+
+		models.ForEach( x => x.Draw( uniformBuffer, commandList ) );
+	}
+}
