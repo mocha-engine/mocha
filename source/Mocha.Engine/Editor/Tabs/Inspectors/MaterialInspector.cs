94f3cae893cafcecea5d742cc07063116505d9d5
Alex <alex@gu3.me>
2022-07-25 03:02:01 +0100

    Improve material inspector by making things previewable

--- source/Mocha.Engine/Editor/Tabs/Inspectors/MaterialInspector.cs
+++ source/Mocha.Engine/Editor/Tabs/Inspectors/MaterialInspector.cs
@@ -11,23 +11,57 @@ public class MaterialInspector : BaseInspector
 		this.material = material;
 	}
 
+	private void TextureSlot( string name, Texture texture )
+	{
+		var startPos = ImGui.GetCursorPos();
+
+		ImGui.TableNextRow();
+		ImGui.TableNextColumn();
+
+		texture ??= TextureBuilder.MissingTexture;
+
+		EditorHelpers.Image( texture, new Vector2( 32f ) );
+		ImGui.TableNextColumn();
+		ImGui.Text( $"{name}\n{texture.Type}" );
+		ImGui.TableNextColumn();
+		ImGui.Text( $"{texture.Path}\n{texture.Width}x{texture.Height}" );
+
+		var rectSize = new System.Numerics.Vector2( 0, 32 );
+		ImGui.SetCursorPos( startPos + new System.Numerics.Vector2( 0, 4 ) );
+
+		if ( ImGui.Selectable( $"##select_{name}", false, ImGuiSelectableFlags.SpanAllColumns, rectSize ) )
+			InspectorTab.SetSelectedObject( texture );
+	}
+
 	public override void Draw()
 	{
+		var windowWidth = ImGui.GetWindowWidth();
+
 		EditorHelpers.Title(
 			$"{FontAwesome.FaceGrinStars} {Path.GetFileName( material.Path )}",
 			"This is a material."
 		);
 
-		var items = new List<(string, string)>();
-		foreach ( var property in material.GetType().GetProperties().Where( x => x.PropertyType == typeof( Texture ) ) )
-		{
-			var texture = property.GetValue( material ) as Texture;
-			items.Add( (EditorHelpers.GetDisplayName( property.Name ), texture?.Path ?? "") );
-		}
+		EditorHelpers.Image( material.DiffuseTexture ?? TextureBuilder.MissingTexture, new Vector2( windowWidth, windowWidth ) - new Vector2( 16, 0 ) );
+		EditorHelpers.Separator();
 
 		EditorHelpers.TextBold( $"{FontAwesome.FaceGrinStars} Material" );
 
-		DrawTable( items.ToArray() );
+		if ( ImGui.BeginTable( $"##material_slots", 3, ImGuiTableFlags.PadOuterX | ImGuiTableFlags.SizingStretchProp ) )
+		{
+			ImGui.TableSetupColumn( "Preview", ImGuiTableColumnFlags.WidthFixed, 32f );
+			ImGui.TableSetupColumn( "Name", ImGuiTableColumnFlags.WidthFixed, 100f );
+			ImGui.TableSetupColumn( "Value", ImGuiTableColumnFlags.WidthStretch, 1f );
+			
+			foreach ( var property in material.GetType().GetProperties().Where( x => x.PropertyType == typeof( Texture ) ) )
+			{
+				var texture = property.GetValue( material ) as Texture;
+
+				TextureSlot( EditorHelpers.GetDisplayName( property.Name ), texture );
+			}
+
+			ImGui.EndTable();
+		}
 
 		EditorHelpers.Separator();
 
