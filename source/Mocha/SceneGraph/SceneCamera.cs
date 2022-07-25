08b102aba7d7ea4af19b75ccc0fc3470bdd00fb1
Alex <alex@gu3.me>
2022-07-25 02:07:13 +0100

    Move SceneCamera depth/color texture creation so it can be used in resizing later

--- source/Mocha/SceneGraph/SceneCamera.cs
+++ source/Mocha/SceneGraph/SceneCamera.cs
@@ -4,51 +4,66 @@ namespace Mocha.Renderer;
 
 public class SceneCamera : SceneObject
 {
-    public Matrix4x4 ViewMatrix { get; set; }
-    public Matrix4x4 ProjMatrix { get; set; }
-    public Framebuffer Framebuffer { get; set; }
+	public Matrix4x4 ViewMatrix { get; set; }
+	public Matrix4x4 ProjMatrix { get; set; }
+	public Framebuffer Framebuffer { get; set; }
 
-    public Texture ColorTexture { get; set; }
-    public Texture DepthTexture { get; set; }
+	public Texture ColorTexture { get; set; }
+	public Texture DepthTexture { get; set; }
 
-    public float FieldOfView { get; set; } = 60;
+	public float FieldOfView { get; set; } = 60;
 
-    public SceneCamera( IEntity entity ) : base( entity )
-    {
-        uint resX = (uint)Window.Current.Size.X;
-        uint resY = (uint)Window.Current.Size.Y;
+	private Point2 CurrentSize { get; set; }
 
-        DepthTexture = Texture.Builder
-            .FromEmpty( resX, resY )
-            .AsDepthAttachment()
-            .WithName( $"Camera {GetHashCode()} depth" )
-            .Build();
+	public SceneCamera( IEntity entity ) : base( entity )
+	{
+		UpdateSize( Window.Current.Size );
+	}
 
-        ColorTexture = Texture.Builder
-            .FromEmpty( resX, resY )
-            .AsColorAttachment()
-            .WithName( $"Camera {GetHashCode()} color" )
-            .Build();
+	// TODO: Make it so that we can call this in ViewportTab etc. without running OOM
+	private void UpdateSize( Point2 newSize )
+	{
+		if ( newSize.X == CurrentSize.X && newSize.Y == CurrentSize.Y )
+			return;
 
-        var framebufferDescription = new FramebufferDescription( DepthTexture.VeldridTexture, ColorTexture.VeldridTexture );
+		Framebuffer?.Dispose();
 
-        Framebuffer = Device.ResourceFactory.CreateFramebuffer( framebufferDescription );
-    }
+		DepthTexture = Texture.Builder
+			.FromEmpty( (uint)newSize.X, (uint)newSize.Y )
+			.AsDepthAttachment()
+			.IgnoreCache()
+			.WithName( $"Camera {GetHashCode()} depth" )
+			.Build();
 
-    public void CalcViewProjMatrix()
-    {
-        FieldOfView = FieldOfView.Clamp( 1, 179 );
+		ColorTexture = Texture.Builder
+			.FromEmpty( (uint)newSize.X, (uint)newSize.Y )
+			.AsColorAttachment()
+			.IgnoreCache()
+			.WithName( $"Camera {GetHashCode()} color" )
+			.Build();
 
-        var cameraPos = Transform.Position;
-        var cameraFront = Transform.Rotation.Forward;
-        var cameraUp = new Vector3( 0, 0, 1 );
+		var framebufferDescription = new FramebufferDescription( DepthTexture.VeldridTexture, ColorTexture.VeldridTexture );
+		Framebuffer = Device.ResourceFactory.CreateFramebuffer( framebufferDescription );
 
-        ViewMatrix = Matrix4x4.CreateLookAt( cameraPos, cameraPos + cameraFront, cameraUp );
-        ProjMatrix = Matrix4x4.CreatePerspectiveFieldOfView(
-            FieldOfView.DegreesToRadians(),
-            Screen.Aspect,
-            0.1f,
-            1000.0f
-        );
-    }
+		Log.Trace( $"Resized to {newSize}" );
+
+		CurrentSize = newSize;
+	}
+
+	public void CalcViewProjMatrix()
+	{
+		FieldOfView = FieldOfView.Clamp( 1, 179 );
+
+		var cameraPos = Transform.Position;
+		var cameraFront = Transform.Rotation.Forward;
+		var cameraUp = new Vector3( 0, 0, 1 );
+
+		ViewMatrix = Matrix4x4.CreateLookAt( cameraPos, cameraPos + cameraFront, cameraUp );
+		ProjMatrix = Matrix4x4.CreatePerspectiveFieldOfView(
+			FieldOfView.DegreesToRadians(),
+			(float)CurrentSize.X / (float)CurrentSize.Y,
+			0.1f,
+			1000.0f
+		);
+	}
 }
