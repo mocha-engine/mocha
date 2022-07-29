837df7303965c555bedc825f0ef75305b0699868
Alex <alex@gu3.me>
2022-07-29 13:57:14 +0100

    Bootstrap the engine using C++ in a really shit way for now, we'll re-visit this later

--- source/Mocha.Engine/Program.cs
+++ source/Mocha.Engine/Program.cs
@@ -1,6 +1,7 @@
 ﻿global using static Mocha.Engine.Global;
 global using Matrix4x4 = System.Numerics.Matrix4x4;
 global using Vector4 = System.Numerics.Vector4;
+using System.Runtime.InteropServices;
 
 namespace Mocha.Engine;
 
@@ -12,5 +13,26 @@ public class Program
 	public static void Main( string[] args )
 	{
 		var game = new Game();
+	}
+
+	[UnmanagedCallersOnly]
+	public static void HostedMain()
+	{
+		Log.Trace( Directory.GetCurrentDirectory() );
+		try
+		{
+			var game = new Game();
+		}
+		catch ( Exception ex )
+		{
+			Console.WriteLine( $"Caught unhandled exception:\n{ex}" );
+		}
+	}
+
+	[UnmanagedCallersOnly]
+	public static void CustomEntryPointUnmanaged( int number )
+	{
+		Console.WriteLine( "Hello world!" );
+		Console.WriteLine( number );
 	}
 }
