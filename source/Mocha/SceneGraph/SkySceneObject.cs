1e33fd2ddcb7f603dbd367c85ee51bfda6172857
Alex <alex@gu3.me>
2022-07-30 12:27:53 +0100

    Modify default atmosphere values so that they look better with a lower step count

--- source/Mocha/SceneGraph/SkySceneObject.cs
+++ source/Mocha/SceneGraph/SkySceneObject.cs
@@ -5,8 +5,8 @@ namespace Mocha.Renderer;
 public class SkySceneObject : ModelSceneObject
 {
     public float SunIntensity { get; set; } = 32.0f;
-    public float PlanetRadius { get; set; } = 6371000;
-    public float AtmosphereRadius { get; set; } = 6381000;
+    public float PlanetRadius { get; set; } = 6372000;
+    public float AtmosphereRadius { get; set; } = 6380000;
 
     public SkySceneObject( IEntity entity ) : base( entity )
     {
