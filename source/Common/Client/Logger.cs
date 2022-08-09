7fa5bb07935e97cfec85d9b79ea5cf62509f93ed
Alex <alex@gu3.me>
2022-08-10 00:44:35 +0100

    More work on editor, try and get float interop working properly, clean up logger

--- source/Common/Client/Logger.cs
+++ source/Common/Client/Logger.cs
@@ -1,63 +1,11 @@
-﻿using System.Diagnostics;
-
-namespace Mocha.Common;
+﻿namespace Mocha.Common;
 
 public class Logger
 {
 	public Glue.CLogger NativeLogger { get; set; }
 
-	public enum Level
-	{
-		Trace,
-		Info,
-		Warning,
-		Error
-	};
-
 	public void Trace( object obj ) => NativeLogger.Trace( obj?.ToString() );
 	public void Info( object obj ) => NativeLogger.Info( obj?.ToString() );
 	public void Warning( object obj ) => NativeLogger.Warning( obj?.ToString() );
 	public void Error( object obj ) => NativeLogger.Error( obj?.ToString() );
-
-	public static Action<Level, string, StackTrace> OnLog;
-
-	private static void InternalLog( string? str, Level severity = Level.Trace )
-	{
-		if ( str == null )
-			return;
-
-		var stackTrace = new System.Diagnostics.StackTrace();
-
-#if RELEASE
-		if ( severity == Level.Error )
-			throw new Exception( str );
-#endif
-
-		Console.Write( $"[{DateTime.Now.ToLongTimeString()}] " );
-
-		Console.ForegroundColor = SeverityToConsoleColor( severity );
-		Console.Write( $"[{severity}] ".Pad() );
-		Console.ForegroundColor = ConsoleColor.Gray;
-
-		Console.WriteLine( $"{str}" );
-
-		OnLog?.Invoke( severity, str, stackTrace );
-	}
-
-	private static ConsoleColor SeverityToConsoleColor( Level severity )
-	{
-		switch ( severity )
-		{
-			case Level.Error:
-				return ConsoleColor.DarkRed;
-			case Level.Warning:
-				return ConsoleColor.Red;
-			case Level.Trace:
-				return ConsoleColor.DarkGray;
-			case Level.Info:
-				return ConsoleColor.White;
-		}
-
-		return ConsoleColor.White;
-	}
 }
--- source/Common/Glue/UnmanagedArgs.cs
+++ source/Common/Glue/UnmanagedArgs.cs
@@ -29,13 +29,23 @@ public struct UnmanagedArgs
     public IntPtr __EditorUI_TextLightMethodPtr;
     public IntPtr __EditorUI_TitleMethodPtr;
     public IntPtr __EditorUI_ButtonMethodPtr;
+    public IntPtr __EditorUI_ButtonExMethodPtr;
+    public IntPtr __EditorUI_PushStyleVarMethodPtr;
+    public IntPtr __EditorUI_PushStyleVarExMethodPtr;
+    public IntPtr __EditorUI_PopStyleVarMethodPtr;
+    public IntPtr __EditorUI_PopStyleVarsMethodPtr;
     public IntPtr __EditorUI_BeginMethodPtr;
     public IntPtr __EditorUI_ShowDemoWindowMethodPtr;
     public IntPtr __EditorUI_BeginMainMenuBarMethodPtr;
     public IntPtr __EditorUI_EndMainMenuBarMethodPtr;
+    public IntPtr __EditorUI_SetCursorPosXMethodPtr;
     public IntPtr __EditorUI_SetCursorPosXRelativeMethodPtr;
+    public IntPtr __EditorUI_SetCursorPosYMethodPtr;
     public IntPtr __EditorUI_SetCursorPosYRelativeMethodPtr;
+    public IntPtr __EditorUI_GetViewportSizeXMethodPtr;
+    public IntPtr __EditorUI_GetViewportSizeYMethodPtr;
     public IntPtr __EditorUI_BeginMenuMethodPtr;
     public IntPtr __EditorUI_EndMenuMethodPtr;
+    public IntPtr __EditorUI_MenuItemExMethodPtr;
     public IntPtr __EditorUI_MenuItemMethodPtr;
 }
