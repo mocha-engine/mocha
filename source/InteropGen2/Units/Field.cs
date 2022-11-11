b926e52779869487ce905d74499585f57c9a16e0
Alex <alex@gu3.me>
2022-11-11 01:38:34 +0000

    Start working on InteropGen2 (uses libclang)

---
+++ source/InteropGen2/Units/Field.cs
@@ -0,0 +1,16 @@
+﻿struct Field
+{
+	public Field( string name, string type ) : this()
+	{
+		Name = name;
+		Type = type;
+	}
+
+	public string Name { get; set; }
+	public string Type { get; set; }
+
+	public override string ToString()
+	{
+		return $"{Type} {Name}";
+	}
+}
---
+++ source/InteropGen2/Units/Structure.cs
@@ -0,0 +1,19 @@
+﻿struct Structure : IUnit
+{
+	public Structure( string name ) : this()
+	{
+		Name = name;
+
+		Fields = new();
+		Methods = new();
+	}
+
+	public string Name { get; set; }
+	public List<Method> Methods { get; set; }
+	public List<Field> Fields { get; set; }
+
+	public override string ToString()
+	{
+		return Name;
+	}
+}
