91d8445326bf71d45a3fa0e63b6e5bab3c68bd59
Alex <alex@gu3.me>
2022-11-16 01:57:36 +0000

    Clean up window stuff

--- source/Host/window.cpp
+++ source/Host/window.cpp
@@ -1,1 +1,44 @@
 #include "window.h"
+
+CWindow::CWindow( uint32_t width, uint32_t height )
+{
+	SDL_Init( SDL_INIT_VIDEO );
+
+	SDL_WindowFlags windowFlags = ( SDL_WindowFlags )( SDL_WINDOW_VULKAN );
+
+	m_window = SDL_CreateWindow( "Mocha", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, windowFlags );
+}
+
+VkSurfaceKHR CWindow::CreateSurface( VkInstance instance )
+{
+	VkSurfaceKHR surface;
+	SDL_Vulkan_CreateSurface( m_window, instance, &surface );
+	return surface;
+}
+
+void CWindow::Cleanup()
+{
+	SDL_DestroyWindow( m_window );
+}
+
+bool CWindow::Update()
+{
+	SDL_Event e;
+	bool bQuit = false;
+
+	while ( SDL_PollEvent( &e ) != 0 )
+	{
+		if ( e.type == SDL_QUIT )
+		{
+			return true;
+		}
+		else if ( e.type == SDL_KEYDOWN )
+		{
+			SDL_KeyboardEvent ke = e.key;
+			char c = SDL_GetKeyFromScancode( ke.keysym.scancode );
+			spdlog::info( "Key down: {}", c );
+		}
+	}
+
+	return false;
+}
