09626ffdd736c82faf3720e76694c8cda541d01d
Alex <alex@gu3.me>
2022-11-21 04:30:20 +0000

    Start cleaning up managed entity stuff

--- source/Engine/World/Camera.cs
+++ source/Engine/World/Camera.cs
@@ -3,40 +3,22 @@
 [Category( "Player" ), Title( "Camera" ), Icon( FontAwesome.Camera )]
 public class Camera : BaseEntity
 {
+	// TODO: Native entities (so we can move this bullshit into BaseEntity)
+	private Glue.Camera NativeCamera { get; set; }
+
 	private new Vector3 Position
 	{
 		set => NativeCamera.SetPosition( value );
 	}
 
-	private Glue.Camera NativeCamera { get; set; }
-
-	[HideInInspector]
-	public SceneCamera SceneCamera { get; set; }
-
-	private float FieldOfView
-	{
-		get => SceneCamera.FieldOfView;
-		set => SceneCamera.FieldOfView = value;
-	}
-
-	[HideInInspector]
-	public Matrix4x4 ProjMatrix => SceneCamera.ProjMatrix;
-
-	[HideInInspector]
-	public Matrix4x4 ViewMatrix => SceneCamera.ViewMatrix;
-
 	public Camera()
 	{
 		NativeCamera = new();
-		SceneCamera = new( this );
 	}
 
 	public override void Update()
 	{
 		base.Update();
-
-		// Apply fov
-		FieldOfView = 50f;
 
 		// Set camera position
 		var newPos = new Vector3();
@@ -44,8 +26,5 @@ public class Camera : BaseEntity
 		newPos.Y = MathF.Sin( Time.Now * 2 * 0.5f ) * 4.0f;
 		newPos.Z = MathF.Cos( Time.Now * 1 * 0.5f ) * 4.0f;
 		Position = newPos;
-
-		// Run view/proj matrix calculations
-		SceneCamera.CalcViewProjMatrix();
 	}
 }
