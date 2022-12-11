ec8a0d32a3fb68aac169dc771d3aaa352ab92182
Alex <alex@gu3.me>
2022-12-11 23:56:45 +0000

    Set friction / restitution / mass in C#, add "spawn a ball" button to editor for testing

--- source/Engine/World/World.cs
+++ source/Engine/World/World.cs
@@ -20,6 +20,9 @@ public class World
 
 		var floor = new ModelEntity( "core/models/dev/dev_map.mmdl" );
 		floor.Position = new Vector3( 0, 0, -0.5f );
+		floor.Friction = 1.0f;
+		floor.Restitution = 0.0f;
+		floor.Mass = 1000.0f;
 		floor.SetCubePhysics( new Vector3( 20f, 20f, 0.5f ), true );
 
 		for ( int x = -4; x < 4; ++x )
@@ -28,6 +31,9 @@ public class World
 			{
 				var ball = new ModelEntity( "core/models/dev/dev_ball.mmdl" );
 				ball.Position = new( x, y, 10.0f + (x + y) );
+				ball.Restitution = 1.0f;
+				ball.Friction = 1.0f;
+				ball.Mass = 10.0f;
 				ball.SetSpherePhysics( 0.5f, false );
 			}
 		}
