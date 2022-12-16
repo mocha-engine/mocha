adc32bf5ff6f4b5c6571551cda640ee8e1bc429b
Alex <alex@gu3.me>
2022-12-16 23:21:39 +0000

    Start working on bringing back all the UI rendering stuff from Veldrid Mocha

---
+++ source/Engine/Render/UI/AtlasBuilder.cs
@@ -0,0 +1,51 @@
+﻿namespace Mocha.Renderer.UI;
+
+public class AtlasBuilder
+{
+	private List<(Point2 Position, Texture Texture)> TextureCache { get; } = new();
+
+	public Texture Texture { get; private set; }
+
+	public Action OnBuild;
+
+	private uint RowHeight = 0;
+	private Vector2 Cursor = new();
+
+	private const uint Size = 8192;
+
+	public AtlasBuilder()
+	{
+		Texture = new Texture( Size, Size );
+	}
+
+	public Point2 AddOrGetTexture( Texture texture )
+	{
+		if ( TextureCache.Any( x => x.Texture == texture ) )
+		{
+			return TextureCache.First( x => x.Texture == texture ).Position;
+		}
+
+		if ( RowHeight < texture.Height )
+			RowHeight = texture.Height;
+
+		if ( Cursor.X + texture.Width > Size )
+		{
+			Cursor.X = 0;
+			Cursor.Y += RowHeight;
+			RowHeight = 0;
+		}
+
+		//
+		// Copy the texture into the atlas
+		//
+		Point2 pos = new Point2( (int)Cursor.X, (int)Cursor.Y );
+
+		// ...
+
+		TextureCache.Add( (pos, texture) );
+		Cursor.X += texture.Width;
+
+		OnBuild?.Invoke();
+		return pos;
+	}
+}
---
+++ source/Engine/Render/UI/PanelRenderer.cs
@@ -0,0 +1,64 @@
+﻿namespace Mocha.Renderer.UI;
+
+[Icon( FontAwesome.Square ), Title( "UI" )]
+public partial class PanelRenderer
+{
+	public AtlasBuilder AtlasBuilder { get; set; }
+	private Material Material { get; set; }
+	private Model Model { get; set; }
+
+	public PanelRenderer()
+	{
+		AtlasBuilder = new();
+
+		Material = new( "core/shaders/ui/ui.mshdr", UIVertex.VertexAttributes );
+	}
+
+	public void NewFrame()
+	{
+		Vertices.Clear();
+		RectCount = 0;
+	}
+
+	public void AddRectangle( Common.Rectangle rect, Common.Rectangle ndcTexRect, float screenPxRange, Vector4 colorA, Vector4 colorB, Vector4 colorC, Vector4 colorD, GraphicsFlags flags )
+	{
+		if ( rect.X > Screen.Size.X || rect.Y > Screen.Size.Y )
+			return;
+
+		var ndcRect = rect / (Vector2)Screen.Size;
+		var vertices = RectVertices.Select( ( x, i ) =>
+		{
+			var position = x.Position;
+			position.X = (x.Position.X * ndcRect.Size.X) + ndcRect.Position.X;
+			position.Y = (x.Position.Y * ndcRect.Size.Y) + ndcRect.Position.Y;
+
+			var texCoords = x.TexCoords;
+			texCoords.X = (x.TexCoords.X * ndcTexRect.Size.X) + ndcTexRect.Position.X;
+			texCoords.Y = (x.TexCoords.Y * ndcTexRect.Size.Y) + ndcTexRect.Position.Y;
+
+			var tx = x;
+			position *= 2.0f;
+			position.X -= 1.0f;
+			position.Y = 1.0f - position.Y;
+
+			tx.Position = position;
+			tx.TexCoords = texCoords;
+			tx.PanelPos *= rect.Size;
+			tx.PanelSize = rect.Size;
+			tx.Color = i switch
+			{
+				0 => colorA,
+				1 => colorB,
+				2 => colorC,
+				3 => colorD,
+				_ => Vector4.Zero,
+			};
+			tx.Flags = (short)flags;
+
+			return tx;
+		} ).ToArray();
+
+		Vertices.AddRange( vertices );
+		RectCount++;
+	}
+}
