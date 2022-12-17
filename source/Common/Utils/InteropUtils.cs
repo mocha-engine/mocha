8c57701da860c38d85999dd61449509d5ada40d6
Alex <alex@gu3.me>
2022-12-17 14:04:14 +0000

    Add InteropArray wrapper for list/array objects, .ToInterop() on any list/array to use it

--- source/Common/Utils/InteropUtils.cs
+++ source/Common/Utils/InteropUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.InteropServices;
 
 namespace Mocha.Glue;
+
 public static class InteropUtils
 {
 	public static IntPtr GetPtr( object obj )
@@ -8,6 +9,10 @@ public static class InteropUtils
 		if ( obj is IntPtr pointer )
 		{
 			return pointer;
+		}
+		else if ( obj is IInteropArray arr )
+		{
+			return GetPtr( arr.GetNative() );
 		}
 		else if ( obj is string str )
 		{
@@ -47,4 +52,53 @@ public static class InteropUtils
 	{
 		return Marshal.PtrToStringUTF8( strPtr );
 	}
+}
+
+public interface IInteropArray
+{
+	public Glue.InteropStruct GetNative();
+}
+
+public class InteropArray<T> : IInteropArray
+{
+	private Glue.InteropStruct NativeStruct;
+	private InteropArray() { }
+
+	public static InteropArray<T> FromArray( T[] array )
+	{
+		var s = new InteropArray<T>();
+		s.NativeStruct = new();
+
+		int stride = Marshal.SizeOf( typeof( T ) );
+		int size = stride * array.Length;
+
+		s.NativeStruct.count = array.Length;
+		s.NativeStruct.size = size;
+
+		unsafe
+		{
+			fixed ( void* data = array )
+			{
+				s.NativeStruct.data = (IntPtr)data;
+			}
+		}
+
+		return s;
+	}
+
+	public static InteropArray<T> FromList( List<T> list )
+	{
+		return FromArray( list.ToArray() );
+	}
+
+	public InteropStruct GetNative()
+	{
+		return NativeStruct;
+	}
+
+	//
+	// Implicit conversions to Glue.InteropStruct and from lists/arrays
+	//
+	public static implicit operator Glue.InteropStruct( InteropArray<T> arr ) => arr.GetNative();
+	public static implicit operator InteropArray<T>( List<T> list ) => FromList( list );
 }
