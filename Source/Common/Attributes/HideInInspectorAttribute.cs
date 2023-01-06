98c3ceaeb35b627ad1f050458ee313ffdd25a85e
peter-r-g <petergorman11@gmail.com>
2023-01-06 13:35:16 -0500

    Minor change to HideInInspectorAttribute

--- Source/Common/Attributes/HideInInspectorAttribute.cs
+++ Source/Common/Attributes/HideInInspectorAttribute.cs
@@ -1,5 +1,9 @@
 ﻿namespace Mocha.Common;
 
+/// <summary>
+/// Marks an item to be hidden in the inspector window.
+/// </summary>
+[AttributeUsage( AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true )]
 public class HideInInspectorAttribute : Attribute
 {
 }
