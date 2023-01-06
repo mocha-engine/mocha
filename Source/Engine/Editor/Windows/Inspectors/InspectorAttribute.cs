86da730b45b636c5449650e907048b4a73e6b437
peter-r-g <petergorman11@gmail.com>
2023-01-06 13:28:55 -0500

    Add InspectorAttribute<T>

---
+++ Source/Engine/Editor/Windows/Inspectors/InspectorAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Mocha.Editor;
+
+/// <summary>
+/// Marks an inspector to be usable for <see ref="T"/>.
+/// </summary>
+/// <typeparam name="T">The type that the inspector can handle.</typeparam>
+[AttributeUsage( AttributeTargets.Class, AllowMultiple = false, Inherited = false )]
+public class InspectorAttribute<T> : Attribute
+{
+}
