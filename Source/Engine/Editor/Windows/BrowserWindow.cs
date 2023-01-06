33f66aa8ec80f13f73d93aa276185eeda52021a7
peter-r-g <petergorman11@gmail.com>
2023-01-06 13:30:14 -0500

    Change BrowserWindow.SetSelectedObject behavior
    The method now uses reflection to find the most suitable inspector for the object provided. If none are found then a warning is logged to console.

--- Source/Engine/Editor/Windows/BrowserWindow.cs
+++ Source/Engine/Editor/Windows/BrowserWindow.cs
@@ -1,4 +1,7 @@
-﻿namespace Mocha.Editor;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace Mocha.Editor;
 
 [Icon( FontAwesome.Folder ), Title( "Browser" ), Category( "Game" )]
 internal class BrowserWindow : EditorWindow
@@ -32,17 +35,66 @@ internal class BrowserWindow : EditorWindow
 
 	private BaseInspector Inspector { get; set; }
 
+	private static Dictionary<Type, Type> inspectorTypeCache = new();
+
+	/// <summary>
+	/// Attempts to get a suitable inspector for <see ref="objType"/>.
+	/// </summary>
+	/// <param name="objType">The type to find the inspector for.</param>
+	/// <param name="type">The type of the inspector that was found suitable.</param>
+	/// <returns>Whether or not a suitable inspector was found.</returns>
+	private static bool TryGetInspector( Type objType, [NotNullWhen( true )] out Type? type )
+	{
+		if ( inspectorTypeCache.TryGetValue( objType, out var cachedType ) )
+		{
+			type = cachedType;
+			return true;
+		}
+
+		var inspectorType = typeof( InspectorAttribute<> ).MakeGenericType( objType );
+		// TODO: Search all assemblies, there could be custom inspectors laying around.
+		var inspectors = Assembly.GetExecutingAssembly().GetTypes()
+			.Where( type => type.IsAssignableTo( typeof( BaseInspector ) ) )
+			.ToList();
+
+		foreach ( var inspector in inspectors )
+		{
+			if ( inspector.GetCustomAttribute( inspectorType ) is null )
+				continue;
+
+			inspectorTypeCache.Add( objType, inspector );
+
+			type = inspector;
+			return true;
+		}
+
+		if ( objType.BaseType is not null )
+		{
+			var result = TryGetInspector( objType.BaseType, out var nestedInspectorType );
+			if ( result )
+				inspectorTypeCache.Add( objType, nestedInspectorType! );
+
+			type = nestedInspectorType;
+			return result;
+		}
+
+		type = null;
+		return false;
+	}
+
+	/// <summary>
+	/// Sets the active item to display in the inspector window.
+	/// </summary>
+	/// <param name="obj">The item to display.</param>
 	public static void SetSelectedObject( object obj )
 	{
-		// TODO: Use reflection for this
-		if ( obj is Texture texture )
-			Instance.Inspector = new TextureInspector( texture );
-		else if ( obj is Model model )
-			Instance.Inspector = new ModelInspector( model );
-		else if ( obj is List<Model> modelList )
-			Instance.Inspector = new ModelInspector( modelList.First() );
-		else if ( obj is Material material )
-			Instance.Inspector = new MaterialInspector( material );
+		if ( !TryGetInspector( obj.GetType(), out var inspectorType ) )
+		{
+			Log.Warning( $"Failed to find an inspector for {obj}" );
+			return;
+		}
+
+		Instance.Inspector = (BaseInspector)Activator.CreateInstance( inspectorType, obj )!;
 	}
 
 	public void SelectItem( string name )
