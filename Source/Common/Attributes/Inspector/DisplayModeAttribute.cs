dc34c086cd0f23ebf53feddea645e4789c01bedf
peter-r-g <petergorman11@gmail.com>
2023-01-12 12:52:16 -0500

    Add DisplayModeAttribute

---
+++ Source/Common/Attributes/Inspector/DisplayModeAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Mocha.Common;
+
+/// <summary>
+/// Marks a property to override the default display method of its property editor with the given option.
+/// </summary>
+[AttributeUsage( AttributeTargets.Property, AllowMultiple = false, Inherited = true )]
+public class DisplayModeAttribute : Attribute
+{
+	public DisplayMode DisplayMode { get; }
+
+	public DisplayModeAttribute( DisplayMode displayMode = DisplayMode.Text )
+	{
+		DisplayMode = displayMode;
+	}
+}
