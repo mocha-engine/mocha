38361ee91bbe03750102cb7707fe73eaf03f41ed
peter-r-g <petergorman11@gmail.com>
2023-01-12 12:52:24 -0500

    Add MinMaxAttribute

---
+++ Source/Common/Attributes/Inspector/MinMaxAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Mocha.Common;
+
+/// <summary>
+/// Marks a property to override the default Min/Max value of the property editor.
+/// </summary>
+[AttributeUsage( AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true )]
+public class MinMaxAttribute : Attribute
+{
+	public int Min { get; }
+	public int Max { get; }
+
+	public MinMaxAttribute( int min, int max )
+	{
+		Min = min;
+		Max = max;
+	}
+
+	public MinMaxAttribute( int max ) : this( 0, max )
+	{
+	}
+}
