ce6d44d705abfad107d0aec516e34ccf77e20051
peter-r-g <petergorman11@gmail.com>
2023-01-12 12:52:42 -0500

    Add ShowInInspectorAttribute

---
+++ Source/Common/Attributes/Inspector/ShowInInspectorAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Mocha.Common;
+
+/// <summary>
+/// Marks a property that would otherwise be hidden in the inspector to be shown instead.
+/// </summary>
+[AttributeUsage( AttributeTargets.Property, AllowMultiple = false, Inherited = true )]
+public class ShowInInspectorAttribute : Attribute
+{
+}
