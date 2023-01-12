15c1b02547c373a9a2162ee71e019225ab0a6903
peter-r-g <petergorman11@gmail.com>
2023-01-12 12:58:47 -0500

    Update ObjectInspector to support property editors

--- Source/Engine/Editor/Windows/Inspectors/ObjectInspector.cs
+++ Source/Engine/Editor/Windows/Inspectors/ObjectInspector.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Data;
+using System.Reflection;
 
 namespace Mocha.Editor;
 
@@ -8,34 +9,88 @@ namespace Mocha.Editor;
 [Inspector<object>]
 public class ObjectInspector : BaseInspector
 {
-	private object obj;
+	/// <summary>
+	/// The type name of the object.
+	/// </summary>
+	private readonly string objectName;
+	/// <summary>
+	/// The fully qualified name of the object.
+	/// </summary>
+	private readonly string objectFullName;
+
+	/// <summary>
+	/// Dictionary containing all the categories of property editors.
+	/// </summary>
+	private readonly Dictionary<string, List<BasePropertyEditor>> propertyEditors;
+
+	private const int paddingSize = 4;
+	private static readonly string padding = new( ' ', paddingSize );
 
 	public ObjectInspector( object obj )
 	{
-		this.obj = obj;
+		var objType = obj.GetType();
+
+		// Setup names.
+		objectName = objType.Name;
+		objectFullName = objType.FullName ?? "Unknown full name";
+
+		// Get all inspectable properties.
+		var publicProperties = objType.GetProperties( BindingFlags.Instance | BindingFlags.Public )
+			.Where( property => property.CanRead && property.GetCustomAttribute<HideInInspectorAttribute>() is null );
+		var nonPublicProperties = objType.GetProperties( BindingFlags.Instance | BindingFlags.NonPublic )
+			.Where( property => property.CanRead && property.GetCustomAttribute<ShowInInspectorAttribute>() is not null );
+
+		var properties = publicProperties.Concat( nonPublicProperties )
+			.OrderBy( property => property.GetCustomAttribute<CategoryAttribute>()?.Category ?? "Uncategorized" )
+			.ToArray();
+
+		propertyEditors = new Dictionary<string, List<BasePropertyEditor>>();
+		for ( var i = 0; i < properties.Length; i++ )
+		{
+			var property = properties[i];
+			var category = property.GetCustomAttribute<CategoryAttribute>()?.Category ?? "Uncategorized";
+			if ( !propertyEditors.ContainsKey( category ) )
+				propertyEditors.Add( category, new List<BasePropertyEditor>() );
+
+			// Found a suitable property editor to display it.
+			if ( TryGetPropertyEditorType( property.PropertyType, out var propertyEditorType ) )
+			{
+				var readOnly = property.GetCustomAttribute<ReadOnlyInInspectorAttribute>() is not null;
+				propertyEditors[category].Add( (BasePropertyEditor)Activator.CreateInstance( propertyEditorType, obj, property, readOnly )! );
+			}
+			// No suitable editor found, display it as a read only string of the value.
+			else
+				propertyEditors[category].Add( (BasePropertyEditor)Activator.CreateInstance( typeof( StringPropertyEditor ), obj, property, true )! );
+		}
 	}
 
 	public override void Draw()
 	{
-		var objType = obj.GetType();
-
+		// Header.
 		ImGuiX.InspectorTitle(
-			objType.Name,
-			objType.FullName ?? "Unknown full name",
+			objectName,
+			objectFullName,
 			ResourceType.Default
 		);
 
-		var objProperties = objType.GetProperties()
-			.Where( property => property.CanRead && property.GetCustomAttribute<HideInInspectorAttribute>() is null )
-			.ToArray();
-		var properties = new (string, string)[objProperties.Length];
+		// Filler.
+		DrawButtons( "Unknown" );
+		ImGuiX.Separator();
 
-		for ( var i = 0; i < properties.Length; i++ )
+		// Properties.
+		ImGui.BeginChild( "##properties", Vector2.Zero, false, ImGuiWindowFlags.AlwaysVerticalScrollbar | ImGuiWindowFlags.NoBackground );
+
+		foreach ( var (editorCategory, propertyEditors) in propertyEditors )
 		{
-			var property = objProperties[i];
-			properties[i] = (property.Name, property.GetValue( obj )?.ToString() ?? "null");
+			ImGuiX.TextBold( padding + editorCategory );
+			ImGuiX.Separator();
+
+			foreach ( var propertyEditor in propertyEditors )
+				propertyEditor.Draw();
+
+			ImGuiX.Separator();
 		}
 
-		DrawProperties( "Properties", properties, "Unknown" );
+		ImGui.EndChild();
 	}
 }
