40e0982b438d43590cd26c916006f23cd81b7497
Alex <alex@gu3.me>
2023-01-16 17:14:08 +0000

    Project organisation changes (see desc)
    - Main projects are prefixed with `Mocha.`, e.g. `Mocha.UI`. The only exception to this is the Engine project, which just uses `Mocha`.
    - Tool projects are prefixed with `MochaTool.`, e.g. `MochaTool.InteropGen`. There are no exceptions to this.
    - The engine now loads the `Hotload` project instead of directly loading `Engine`. The plan for this is that `Hotload` will load a game assembly that references `Mocha.Common`, `Mocha.Engine`, `Mocha.UI`, etc. and then handle the recompilation and reloading of that game assembly whenever a source file changes.

---
+++ Source/Engine/Game.cs
@@ -0,0 +1,67 @@
+﻿using Mocha.AssetCompiler;
+using VConsoleLib;
+
+namespace Mocha;
+
+public class Game
+{
+	private World world;
+	private VConsoleServer vconsoleServer;
+
+	private void InitFileSystem()
+	{
+		FileSystem.Game = new FileSystem( "content\\" );
+		FileSystem.Game.AssetCompiler = new RuntimeAssetCompiler();
+	}
+
+	private void InitVConsole()
+	{
+		vconsoleServer = new();
+		Log.OnLog += ( s ) => vconsoleServer.Log( s );
+
+		vconsoleServer.OnCommand += ( s ) =>
+		{
+			Log.Info( $"Command: {s}" );
+		};
+	}
+
+	private void InitImGui()
+	{
+		ImGuiNative.igSetCurrentContext( Glue.Editor.GetContextPointer() );
+	}
+
+	private void InitGame()
+	{
+		world = new World();
+	}
+
+	public void Startup()
+	{
+		InitFileSystem();
+		InitVConsole();
+		InitImGui();
+		InitGame();
+	}
+
+	public void Update()
+	{
+		Time.UpdateFrom( Glue.Engine.GetTickDeltaTime() );
+
+		world.Update();
+	}
+
+	public void Render()
+	{
+		Time.UpdateFrom( Glue.Engine.GetDeltaTime() );
+		Screen.UpdateFrom( Glue.Editor.GetRenderSize() );
+		Input.Update();
+
+		world.Render();
+		world.FrameUpdate();
+	}
+
+	public void DrawEditor()
+	{
+		Editor.Editor.Draw();
+	}
+}
---
+++ Source/Hotload/Main.cs
@@ -0,0 +1,48 @@
+﻿using Mocha.Common;
+using System.Runtime.InteropServices;
+
+namespace Mocha;
+
+public static class Main
+{
+	private static Game game;
+	private static World world;
+
+	[UnmanagedCallersOnly]
+	public static void Run( IntPtr args )
+	{
+		Mocha.Common.Global.UnmanagedArgs = Marshal.PtrToStructure<UnmanagedArgs>( args );
+
+		game = new();
+		game.Startup();
+	}
+
+	[UnmanagedCallersOnly]
+	public static void Update()
+	{
+		game.Update();
+	}
+
+	[UnmanagedCallersOnly]
+	public static void Render()
+	{
+		game.Render();
+	}
+
+	[UnmanagedCallersOnly]
+	public static void DrawEditor()
+	{
+		game.DrawEditor();
+	}
+
+	[UnmanagedCallersOnly]
+	public static void FireEvent( IntPtr ptrEventName )
+	{
+		var eventName = Marshal.PtrToStringUTF8( ptrEventName );
+
+		if ( eventName == null )
+			return;
+
+		Event.Run( eventName );
+	}
+}
