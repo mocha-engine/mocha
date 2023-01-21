4680628340d5250dcba65d61f054de210447ab0f
Alex <alex@gu3.me>
2023-01-21 14:08:46 +0000

    Create `BaseGame` class to reduce boilerplate
    - Games can derive from this and `BaseGame` will handle :
      + Entity updates
      + Entity frame updates
      + Registers events & runs Event.Game.Load
      + Handles singleton stuff for you so you can just do `BaseGame.Current` without worrying about it yourself

---
+++ Source/Engine/BaseGame.cs
@@ -0,0 +1,35 @@
+﻿using Mocha.UI;
+
+namespace Mocha;
+
+public class BaseGame : IGame
+{
+	public static BaseGame Current { get; set; }
+
+	public BaseGame()
+	{
+		Current = this;
+		Event.Register( this );
+		Event.RegisterStatics();
+		Event.Run( Event.Game.LoadAttribute.Name );
+	}
+
+	public virtual void FrameUpdate()
+	{
+		UIManager.Instance.Render();
+		BaseEntity.All.ToList().ForEach( entity => entity.FrameUpdate() );
+	}
+
+	public virtual void Update()
+	{
+		BaseEntity.All.ToList().ForEach( entity => entity.Update() );
+	}
+
+	public virtual void Shutdown()
+	{
+	}
+
+	public virtual void Startup()
+	{
+	}
+}
