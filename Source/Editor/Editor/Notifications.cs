bf59006b8a10a202600ed610e40178d3e1eea15c
Alex <alex@gu3.me>
2023-01-21 23:20:31 +0000

    Editor: implement notifications
    fixup

---
+++ Source/Editor/Editor/Notifications.cs
@@ -0,0 +1,75 @@
+﻿namespace Mocha.Editor;
+
+public static partial class Notifications
+{
+	private static void DrawNotifications()
+	{
+		var windowFlags = ImGuiWindowFlags.NoDecoration |
+			ImGuiWindowFlags.AlwaysAutoResize |
+			ImGuiWindowFlags.NoSavedSettings |
+			ImGuiWindowFlags.NoFocusOnAppearing |
+			ImGuiWindowFlags.NoNav |
+			ImGuiWindowFlags.NoInputs |
+			ImGuiWindowFlags.NoDocking |
+			ImGuiWindowFlags.NoMove;
+
+		const float padding = 8.0f;
+
+		var viewport = ImGui.GetMainViewport();
+		var workPos = viewport.WorkPos;
+		var workSize = viewport.WorkSize;
+
+		System.Numerics.Vector2 windowPos;
+
+		windowPos.X = workPos.X + workSize.X - padding;
+		windowPos.Y = workPos.Y + padding;
+
+		float y = 0;
+
+		var notifications = Common.Notify.Notifications.ToArray();
+		for ( int i = 0; i < notifications.Length; i++ )
+		{
+			var notification = notifications[i];
+			if ( notification.Lifetime > 5 )
+				continue;
+
+			float t0 = notification.Lifetime.Relative.LerpInverse( 0.5f, 0.0f );
+			float t1 = notification.Lifetime.Relative.LerpInverse( 4.5f, 5.0f );
+			float alpha = 1.0f - (t0 + t1).Clamp( 0, 1 );
+
+			t0 = EasingFunctions.InExpo( t0 );
+			float t = t0.Clamp( 0, 1 );
+
+			float xOffset = 1.0f - t;
+			var windowPivot = new System.Numerics.Vector2( xOffset, 0 );
+
+			ImGui.PushStyleColor( ImGuiCol.WindowBg, new Vector4( 0, 0, 0, 1 ) );
+			ImGui.PushStyleVar( ImGuiStyleVar.WindowBorderSize, 1 );
+			ImGui.SetNextWindowPos( windowPos + new System.Numerics.Vector2( 0, y ), ImGuiCond.Always, windowPivot );
+			ImGui.SetNextWindowBgAlpha( 0.0f.LerpTo( 1, alpha ) );
+			ImGui.SetNextWindowSize( new System.Numerics.Vector2( 0, 0 ) );
+			ImGui.SetNextWindowViewport( ImGui.GetMainViewport().ID );
+
+			if ( ImGui.Begin( $"##{notification.GetHashCode()}_overlay", windowFlags ) )
+			{
+				ImGui.PushStyleColor( ImGuiCol.Text, new Vector4( 1, 1, 1, alpha ) );
+
+				ImGuiX.TextBold( notification.Title );
+				ImGui.Text( notification.Text );
+
+				ImGui.PopStyleColor();
+
+				y += ImGui.GetWindowSize().Y + 8;
+				ImGui.End();
+			}
+
+			ImGui.PopStyleVar();
+			ImGui.PopStyleColor();
+		}
+	}
+
+	public static void Render()
+	{
+		DrawNotifications();
+	}
+}
--- Source/Editor/Game.cs
+++ Source/Editor/Game.cs
@@ -5,6 +5,7 @@ public class Game : IGame
 	public void FrameUpdate()
 	{
 		DebugOverlay.Render();
+		Notifications.Render();
 		Editor.Draw();
 	}
 
