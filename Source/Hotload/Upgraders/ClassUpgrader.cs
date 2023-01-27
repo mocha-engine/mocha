7207eee41826ebe65e0ad96c3399693dc7a5a5b1
Alex <alex@gu3.me>
2023-01-27 12:44:15 +0000

    Fix #28 by checking for derived type if member is abstract

--- Source/Hotload/Upgraders/ClassUpgrader.cs
+++ Source/Hotload/Upgraders/ClassUpgrader.cs
@@ -19,8 +19,17 @@ public class ClassUpgrader : IMemberUpgrader
 		if ( oldValue == null )
 			return;
 
+		Type type = newMember.Type;
+
+		if ( type.IsAbstract )
+		{
+			var oldDerivedType = oldValue.GetType();
+
+			type = newMember.Type.Assembly.GetTypes().First( x => x.FullName == oldDerivedType.FullName );
+		}
+
 		// Create a new instance of the class WITHOUT calling the constructor
-		object newValue = FormatterServices.GetUninitializedObject( newMember.Type );
+		object newValue = FormatterServices.GetUninitializedObject( type );
 
 		Log.Trace( $"Upgrading class '{oldMember.Type.Name}' (assembly hash {oldMember.Type.Assembly.GetHashCode()}) to '{newMember.Type.Name}' (assembly hash {newMember.Type.Assembly.GetHashCode()})" );
 
