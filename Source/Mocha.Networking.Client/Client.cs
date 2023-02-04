3c0389f814044a1700542e943b0b8a68f5c1dfb6
Alex <alex@gu3.me>
2023-02-04 02:36:08 +0000

    Set up networking client/server projects

---
+++ Source/Mocha.Networking.Client/Client.cs
@@ -0,0 +1,30 @@
+﻿using Steamworks;
+using Steamworks.Data;
+
+namespace Mocha.Networking.Client;
+
+public class ClientConnection : ConnectionManager
+{
+	public override void OnConnecting( ConnectionInfo info )
+	{
+		Console.WriteLine( $"Connecting to {info.Address}" );
+	}
+
+	public override void OnConnected( ConnectionInfo info )
+	{
+		Console.WriteLine( $"Connected to {info.Address}" );
+	}
+
+	public override void OnDisconnected( ConnectionInfo info )
+	{
+		Console.WriteLine( $"Disconnected from {info.Address}" );
+	}
+
+	public override void OnMessage( nint data, int size, long messageNum, long recvTime, int channel )
+	{
+		Console.WriteLine( $"We got a message on channel {channel}!" );
+
+		// Send data back
+		Connection.SendMessage( data, size, SendType.Reliable );
+	}
+}
---
+++ Source/Mocha.Networking/Server.cs
@@ -0,0 +1,31 @@
+﻿using Steamworks;
+using Steamworks.Data;
+
+namespace Mocha.Networking;
+
+public class ServerConnection : SocketManager
+{
+	public override void OnConnecting( Connection connection, ConnectionInfo data )
+	{
+		connection.Accept();
+		Console.WriteLine( $"{data.Identity} is connecting" );
+	}
+
+	public override void OnConnected( Connection connection, ConnectionInfo data )
+	{
+		Console.WriteLine( $"{data.Identity} has joined the game" );
+	}
+
+	public override void OnDisconnected( Connection connection, ConnectionInfo data )
+	{
+		Console.WriteLine( $"{data.Identity} is out of here" );
+	}
+
+	public override void OnMessage( Connection connection, NetIdentity identity, IntPtr data, int size, long messageNum, long recvTime, int channel )
+	{
+		Console.WriteLine( $"We got a message from {identity}!" );
+
+		// Send it right back
+		connection.SendMessage( data, size, SendType.Reliable );
+	}
+}
