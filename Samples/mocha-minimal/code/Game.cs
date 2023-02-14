c612c889599876f8f8e82ea374445a4196917729
Alex <alex@gu3.me>
2023-02-14 23:24:59 +0000

    Only spawn entities on server, only spawn UI on client

--- Samples/mocha-minimal/code/Game.cs
+++ Samples/mocha-minimal/code/Game.cs
@@ -10,27 +10,35 @@ public class Game : BaseGame
 
 	public override void OnStartup()
 	{
-		// Set up UI
-		Hud = new UIManager();
-		Hud.SetTemplate( "ui/Game.html" );
+		if ( Core.IsServer )
+		{
+			// We only want to create these entities on the server.
+			// They will automatically be replicated to clients.
 
-		// Spawn a model to walk around in
-		var map = new ModelEntity( "models/dev/dev_map.mmdl" );
-		map.SetMeshPhysics( "models/dev/dev_map.mmdl" );
+			// Spawn a model to walk around in
+			var map = new ModelEntity( "models/dev/dev_map.mmdl" );
+			map.SetMeshPhysics( "models/dev/dev_map.mmdl" );
 
-		// Spawn a player
-		var player = new Player();
+			// Spawn a player
+			var player = new Player();
+		}
+		else
+		{
+			// UI is client-only
+			Hud = new UIManager();
+			Hud.SetTemplate( "ui/Game.html" );
+		}
 	}
 
 	[Event.Tick, ServerOnly]
 	public void Server()
 	{
-		Log.Info( "Hello I am running on the server" );
+		DebugOverlay.ScreenText( "Server Tick..." );
 	}
 
 	[Event.Tick, ClientOnly]
 	public void Client()
 	{
-		Log.Info( "Hello I am running on the client" );
+		DebugOverlay.ScreenText( "Client Tick..." );
 	}
 }
