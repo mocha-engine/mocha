453ddfabf68ae5417c7f98ff4243758ecba9f14e
Alex <alex@gu3.me>
2023-02-19 15:17:20 +0000

    Basic outline for networkable types, implicitly converted to/from C# primitives

---
+++ Source/Mocha.Networking/Types/INetType.cs
@@ -0,0 +1,10 @@
+﻿namespace Mocha.Networking;
+
+public interface INetType<T> where T : notnull
+{
+	T Value { get; set; }
+
+	// TODO: NetWriter/NetReader?
+	byte[] Serialize();
+	void Deserialize( byte[] values );
+}
---
+++ Source/Mocha.Networking/Types/NetString.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Mocha.Networking;
+
+public class NetString : INetType<string>
+{
+	/// <summary>
+	/// Backing value
+	/// </summary>
+	public string Value { get; set; }
+
+	private NetString( string value )
+	{
+		Value = value;
+	}
+
+	//
+	// Implicit conversions to/from string
+	//
+	public static implicit operator string( NetString netString )
+	{
+		return netString.Value;
+	}
+
+	public static implicit operator NetString( string value )
+	{
+		return new NetString( value );
+	}
+
+	//
+	// Serialization functions
+	//
+	public byte[] Serialize()
+	{
+		return Encoding.UTF8.GetBytes( Value );
+	}
+
+	public void Deserialize( byte[] values )
+	{
+		Value = Encoding.UTF8.GetString( values );
+	}
+}
