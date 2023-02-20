dfbcf7f3b0f439c2f614cc8240e7b19db1bee00d
Alex <alex@gu3.me>
2023-02-20 02:26:34 +0000

    Implement NetFloat (wonder if we could codegen some of these?)

---
+++ Source/Mocha.Networking/Types/NetFloat.cs
@@ -0,0 +1,41 @@
+﻿namespace Mocha.Networking;
+
+[HandlesNetworkedType<float>]
+public class NetFloat : INetType<float>
+{
+	/// <summary>
+	/// Backing value
+	/// </summary>
+	public float Value { get; set; }
+
+	private NetFloat( float value )
+	{
+		Value = value;
+	}
+
+	//
+	// Implicit conversions
+	//
+	public static implicit operator float( NetFloat netString )
+	{
+		return netString.Value;
+	}
+
+	public static implicit operator NetFloat( float value )
+	{
+		return new NetFloat( value );
+	}
+
+	//
+	// Serialization functions
+	//
+	public void Serialize( BinaryWriter binaryWriter )
+	{
+		binaryWriter.Write( Value );
+	}
+
+	public void Deserialize( BinaryReader binaryReader )
+	{
+		Value = binaryReader.ReadSingle();
+	}
+}
