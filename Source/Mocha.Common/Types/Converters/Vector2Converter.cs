26f414f37ef7597ce86056bc8ec979fc638f2854
Alex <alex@gu3.me>
2023-02-23 00:00:07 +0000

    Add Quaternion, Vector2, Vector3 JSON converters; move all converters into folder

---
+++ Source/Mocha.Common/Types/Converters/Vector2Converter.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Mocha.Common;
+
+public class Vector2Converter : JsonConverter<Vector2>
+{
+	public override Vector2 Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
+	{
+		// Read a JSON object with two properties: X and Y
+		if ( reader.TokenType != JsonTokenType.StartObject )
+			throw new JsonException();
+
+		reader.Read();
+		if ( reader.TokenType != JsonTokenType.PropertyName || reader.GetString() != "X" )
+			throw new JsonException();
+
+		reader.Read();
+		var x = reader.GetSingle();
+
+		reader.Read();
+		if ( reader.TokenType != JsonTokenType.PropertyName || reader.GetString() != "Y" )
+			throw new JsonException();
+
+		reader.Read();
+		var y = reader.GetSingle();
+
+		// Skip the end object token
+		reader.Read();
+
+		return new Vector2 { X = x, Y = y };
+	}
+
+	public override void Write( Utf8JsonWriter writer, Vector2 value, JsonSerializerOptions options )
+	{
+		// Write a JSON object with two properties: X and Y
+		writer.WriteStartObject();
+		writer.WriteNumber( "X", value.X );
+		writer.WriteNumber( "Y", value.Y );
+		writer.WriteEndObject();
+	}
+}
---
+++ Source/Mocha.Common/Types/Converters/Vector3Converter.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Mocha.Common;
+
+public class Vector3Converter : JsonConverter<Vector3>
+{
+	public override Vector3 Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
+	{
+		// Read a JSON object with three properties: X, Y and Z
+		if ( reader.TokenType != JsonTokenType.StartObject )
+			throw new JsonException();
+
+		reader.Read();
+		if ( reader.TokenType != JsonTokenType.PropertyName || reader.GetString() != "X" )
+			throw new JsonException();
+
+		reader.Read();
+		var x = reader.GetSingle();
+
+		reader.Read();
+		if ( reader.TokenType != JsonTokenType.PropertyName || reader.GetString() != "Y" )
+			throw new JsonException();
+
+		reader.Read();
+		var y = reader.GetSingle();
+
+		reader.Read();
+		if ( reader.TokenType != JsonTokenType.PropertyName || reader.GetString() != "Z" )
+			throw new JsonException();
+
+		reader.Read();
+		var z = reader.GetSingle();
+
+		// Skip the end object token
+		reader.Read();
+
+		return new Vector3 { X = x, Y = y, Z = z };
+	}
+
+	public override void Write( Utf8JsonWriter writer, Vector3 value, JsonSerializerOptions options )
+	{
+		// Write a JSON object with three properties: X, Y and Z
+		writer.WriteStartObject();
+		writer.WriteNumber( "X", value.X );
+		writer.WriteNumber( "Y", value.Y );
+		writer.WriteNumber( "Z", value.Z );
+		writer.WriteEndObject();
+	}
+}
