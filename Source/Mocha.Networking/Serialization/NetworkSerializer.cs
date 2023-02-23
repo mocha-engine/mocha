9b08cd96e079fb8a4008ab8631ec83a3d8da819c
Alex <alex@gu3.me>
2023-02-23 15:55:34 +0000

    Compress JSON data inside NetworkSerializer - we should probably be serializing as some binary format though

--- Source/Mocha.Networking/Serialization/NetworkSerializer.cs
+++ Source/Mocha.Networking/Serialization/NetworkSerializer.cs
@@ -6,6 +6,9 @@ namespace Mocha.Networking;
 
 internal static class NetworkSerializer
 {
+	private const bool UseCompression = true;
+	private static JsonSerializerOptions s_serializerOptions = CreateJsonSerializerOptions();
+
 	private static JsonSerializerOptions CreateJsonSerializerOptions()
 	{
 		var deserializeOptions = new JsonSerializerOptions();
@@ -20,25 +23,32 @@ internal static class NetworkSerializer
 		return deserializeOptions;
 	}
 
-	private static JsonSerializerOptions SerializerOptions = CreateJsonSerializerOptions();
-
 	public static byte[] Serialize( object obj )
 	{
-		var bytes = JsonSerializer.SerializeToUtf8Bytes( obj, SerializerOptions );
+		var bytes = JsonSerializer.SerializeToUtf8Bytes( obj, s_serializerOptions );
 
 		if ( Core.IsServer )
 			Log.Info( $"Serialized data as {Encoding.UTF8.GetString( bytes )}" );
 
-		return bytes;
+		if ( UseCompression )
+			return Serializer.Compress( bytes );
+		else
+			return bytes;
 	}
 
 	public static T Deserialize<T>( byte[] data )
 	{
-		return JsonSerializer.Deserialize<T>( data, SerializerOptions );
+		if ( UseCompression )
+			data = Serializer.Decompress( data );
+
+		return JsonSerializer.Deserialize<T>( data, s_serializerOptions );
 	}
 
 	public static object? Deserialize( byte[] data, Type type )
 	{
-		return JsonSerializer.Deserialize( data, type, SerializerOptions );
+		if ( UseCompression )
+			data = Serializer.Decompress( data );
+
+		return JsonSerializer.Deserialize( data, type, s_serializerOptions );
 	}
 }
