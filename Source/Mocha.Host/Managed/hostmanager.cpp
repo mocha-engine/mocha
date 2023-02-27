d791f638c0636fb85d7b6f93a8ffbeb8e66f8226
Alex <alex@gu3.me>
2023-02-27 14:11:37 +0000

    Big HostManager cleanup

--- Source/Mocha.Host/Managed/hostmanager.cpp
+++ Source/Mocha.Host/Managed/hostmanager.cpp
@@ -2,14 +2,14 @@
 
 #include <Misc/cvarmanager.h>
 
-void* HostGlobals::load_library( const char_t* path )
+void* HostGlobals::LoadHostLibrary( const char_t* path )
 {
-	HMODULE h = ::LoadLibraryW( path );
+	const HMODULE h = ::LoadLibraryW( path );
 	assert( h != nullptr );
 	return ( void* )h;
 }
 
-void* HostGlobals::get_export( void* h, const char* name )
+void* HostGlobals::GetExport( void* h, const char* name )
 {
 	void* f = ::GetProcAddress( ( HMODULE )h, name );
 	assert( f != nullptr );
@@ -21,18 +21,22 @@ bool HostGlobals::LoadHostFxr()
 	// Pre-allocate a large buffer for the path to hostfxr
 	char_t buffer[MAX_PATH];
 	size_t buffer_size = sizeof( buffer ) / sizeof( char_t );
-	int rc = get_hostfxr_path( buffer, &buffer_size, nullptr );
-	if ( rc != 0 )
+
+	const int getHostfxrPathResult = get_hostfxr_path( buffer, &buffer_size, nullptr );
+	if (getHostfxrPathResult != 0)
 		return false;
 
 	// Load hostfxr and get desired exports
-	void* lib = load_library( buffer );
-	init_fptr = ( hostfxr_initialize_for_runtime_config_fn )get_export( lib, "hostfxr_initialize_for_runtime_config" );
-	get_delegate_fptr = ( hostfxr_get_runtime_delegate_fn )get_export( lib, "hostfxr_get_runtime_delegate" );
-	set_property_fptr = ( hostfxr_set_runtime_property_value_fn )get_export( lib, "hostfxr_set_runtime_property_value" );
-	close_fptr = ( hostfxr_close_fn )get_export( lib, "hostfxr_close" );
+	void* lib = LoadHostLibrary( buffer );
+	init = static_cast<hostfxr_initialize_for_runtime_config_fn>( GetExport( lib,
 
-	return ( init_fptr && get_delegate_fptr && close_fptr );
+		"hostfxr_initialize_for_runtime_config" ) );
+	getDelegate = static_cast<hostfxr_get_runtime_delegate_fn>( GetExport( lib, "hostfxr_get_runtime_delegate" ) );
+	setProperty = static_cast<hostfxr_set_runtime_property_value_fn>( GetExport( lib,
+		"hostfxr_set_runtime_property_value" ) );
+	close = static_cast<hostfxr_close_fn>( GetExport( lib, "hostfxr_close" ) );
+
+	return ( init && getDelegate && close );
 }
 
 load_assembly_and_get_function_pointer_fn HostGlobals::GetDotnetLoadAssembly( const char_t* configPath )
@@ -42,21 +46,21 @@ load_assembly_and_get_function_pointer_fn HostGlobals::GetDotnetLoadAssembly( co
 	// Load .NET Core
 	void* load_assembly_and_get_function_pointer = nullptr;
 	hostfxr_handle cxt = nullptr;
-	int rc = init_fptr( configPath, nullptr, &cxt );
-	if ( rc != 0 || cxt == nullptr )
+	int rc = init( configPath, nullptr, &cxt );
+	if (rc != 0 || cxt == nullptr)
 	{
 		spdlog::error( "Failed to initialize: 0x{:x}", rc );
-		close_fptr( cxt );
+		close( cxt );
 		return nullptr;
 	}
 
 	// Get current working directory
 	char_t cwd[MAX_PATH];
-	DWORD cwd_len = GetCurrentDirectoryW( MAX_PATH, cwd );
-	if ( cwd_len == 0 )
+	const DWORD cwdLength = GetCurrentDirectoryW( MAX_PATH, cwd );
+	if (cwdLength == 0)
 	{
 		spdlog::error( "Failed to get current directory" );
-		close_fptr( cxt );
+		close( cxt );
 		return nullptr;
 	}
 
@@ -65,19 +69,19 @@ load_assembly_and_get_function_pointer_fn HostGlobals::GetDotnetLoadAssembly( co
 	buildPath += L"\\build";
 
 	// Set CoreCLR properties
-	set_property_fptr( cxt, L"APP_CONTEXT_BASE_DIRECTORY", buildPath.c_str() );
-	set_property_fptr( cxt, L"APP_PATHS", buildPath.c_str() );
-	set_property_fptr( cxt, L"APP_NI_PATHS", buildPath.c_str() );
-	set_property_fptr( cxt, L"NATIVE_DLL_SEARCH_DIRECTORIES", buildPath.c_str() );
-	set_property_fptr( cxt, L"PLATFORM_RESOURCE_ROOTS", buildPath.c_str() );
+	setProperty( cxt, L"APP_CONTEXT_BASE_DIRECTORY", buildPath.c_str() );
+	setProperty( cxt, L"APP_PATHS", buildPath.c_str() );
+	setProperty( cxt, L"APP_NI_PATHS", buildPath.c_str() );
+	setProperty( cxt, L"NATIVE_DLL_SEARCH_DIRECTORIES", buildPath.c_str() );
+	setProperty( cxt, L"PLATFORM_RESOURCE_ROOTS", buildPath.c_str() );
 
 	// Get the load assembly function pointer
-	rc = get_delegate_fptr( cxt, hdt_load_assembly_and_get_function_pointer, &load_assembly_and_get_function_pointer );
-	if ( rc != 0 || load_assembly_and_get_function_pointer == nullptr )
+	rc = getDelegate( cxt, hdt_load_assembly_and_get_function_pointer, &load_assembly_and_get_function_pointer );
+	if (rc != 0 || load_assembly_and_get_function_pointer == nullptr)
 		spdlog::error( "Get delegate failed: 0x{:x}", rc );
 
-	close_fptr( cxt );
-	return ( load_assembly_and_get_function_pointer_fn )load_assembly_and_get_function_pointer;
+	close( cxt );
+	return static_cast<load_assembly_and_get_function_pointer_fn>( load_assembly_and_get_function_pointer );
 }
 
 HostManager::HostManager()
@@ -90,21 +94,26 @@ HostManager::HostManager()
 	m_configPath = basePath + L".runtimeconfig.json";
 	m_signature = signature;
 
-	if ( !IsAssemblyLoaded.load() )
+	if (!IsAssemblyLoaded.load())
 	{
 		IsAssemblyLoaded.store( true );
 		LoadFnPtr.store( HostGlobals::GetDotnetLoadAssembly( m_configPath.c_str() ) );
 	}
 }
 
-void HostManager::Update()
+void HostManager::Update() const
 {
 	Invoke( "Update" );
 }
 
-void HostManager::Render()
+void HostManager::Render() const
 {
 	Invoke( "Render" );
+}
+
+void HostManager::DrawEditor() const
+{
+	Invoke( "DrawEditor" );
 }
 
 void HostManager::Startup()
@@ -112,14 +121,9 @@ void HostManager::Startup()
 	Invoke( "Run", ( void* )&args );
 }
 
-void HostManager::DrawEditor()
-{
-	Invoke( "DrawEditor" );
-}
-
 void HostManager::Shutdown() {}
 
-void HostManager::FireEvent( std::string eventName )
+void HostManager::FireEvent( std::string eventName ) const
 {
 	Invoke( "FireEvent", ( void* )eventName.c_str() );
 }
@@ -149,7 +153,7 @@ void HostManager::DispatchIntCVarCallback( CVarManagedVarDispatchInfo<int> info 
 	Invoke( "DispatchIntCVarCallback", &info );
 }
 
-void HostManager::InvokeCallback( Handle callbackHandle, int argsCount, void* args )
+void HostManager::InvokeCallback( Handle callbackHandle, int argsCount, void* args ) const
 {
 	ManagedCallbackDispatchInfo dispatchInfo{};
 	dispatchInfo.args = args;
@@ -159,22 +163,22 @@ void HostManager::InvokeCallback( Handle callbackHandle, int argsCount, void* ar
 	Invoke( "InvokeCallback", &dispatchInfo );
 }
 
-inline void HostManager::Invoke( std::string _method, void* params, const char_t* delegateTypeName )
+inline void HostManager::Invoke( std::string _method, void* params, const char_t* delegateTypeName ) const
 {
 	// Convert to std::wstring
-	std::wstring method( _method.begin(), _method.end() );
+	const std::wstring method( _method.begin(), _method.end() );
 
 	// Function pointer to managed delegate
 	void* fnPtr = nullptr;
 
 	int rc =
-	    LoadFnPtr.load()( m_dllPath.c_str(), m_signature.c_str(), method.c_str(), delegateTypeName, nullptr, ( void** )&fnPtr );
+		LoadFnPtr.load()( m_dllPath.c_str(), m_signature.c_str(), method.c_str(), delegateTypeName, nullptr, ( void** )&fnPtr );
 
-	if ( fnPtr == nullptr )
+	if (fnPtr == nullptr)
 	{
 		spdlog::error( "Failed to load managed method {}", _method );
 	}
 
 	// Invoke method
-	( ( void ( * )( void* ) )fnPtr )( params );
+	static_cast<void ( *)( void* )>( fnPtr )( params );
 }
--- Source/Mocha.Host/Managed/hostmanager.h
+++ Source/Mocha.Host/Managed/hostmanager.h
@@ -16,7 +16,7 @@
 
 using string_t = std::basic_string<char_t>;
 
-typedef int( CORECLR_DELEGATE_CALLTYPE* run_fn )( UnmanagedArgs* args );
+typedef int( CORECLR_DELEGATE_CALLTYPE* RunFn )( UnmanagedArgs* args );
 
 struct CVarManagedCmdDispatchInfo;
 
@@ -26,13 +26,13 @@ struct CVarManagedVarDispatchInfo;
 namespace HostGlobals
 {
 	// Globals to hold hostfxr exports
-	inline hostfxr_initialize_for_runtime_config_fn init_fptr;
-	inline hostfxr_get_runtime_delegate_fn get_delegate_fptr;
-	inline hostfxr_set_runtime_property_value_fn set_property_fptr;
-	inline hostfxr_close_fn close_fptr;
+	inline hostfxr_initialize_for_runtime_config_fn init;
+	inline hostfxr_get_runtime_delegate_fn getDelegate;
+	inline hostfxr_set_runtime_property_value_fn setProperty;
+	inline hostfxr_close_fn close;
 
-	void* load_library( const char_t* path );
-	void* get_export( void* h, const char* name );
+	void* LoadHostLibrary( const char_t* path );
+	void* GetExport( void* h, const char* name );
 	bool LoadHostFxr();
 	load_assembly_and_get_function_pointer_fn GetDotnetLoadAssembly( const char_t* configPath );
 
@@ -44,34 +44,30 @@ inline static std::atomic<load_assembly_and_get_function_pointer_fn> LoadFnPtr;
 class HostManager : ISubSystem
 {
 private:
-	load_assembly_and_get_function_pointer_fn m_lagfp;
+	load_assembly_and_get_function_pointer_fn m_loadAssemblyFunction;
 
 	std::wstring m_dllPath;
 	std::wstring m_configPath;
 	std::wstring m_signature;
 
-	void Invoke( std::string _method, void* params = nullptr, const char_t* delegateTypeName = UNMANAGEDCALLERSONLY_METHOD );
+	void Invoke( std::string _method, void* params = nullptr, const char_t* delegateTypeName = UNMANAGEDCALLERSONLY_METHOD ) const;
 public:
 	HostManager();
 
 	void Startup();
 	void Shutdown();
 
-	void Update();
-	void Render();
-	void DrawEditor();
-	void FireEvent( std::string eventName );
+	void Update() const;
+	void Render() const;
+	void DrawEditor() const;
+	
+	void FireEvent( std::string eventName ) const;
+	void InvokeCallback( Handle callbackHandle, int argsCount, void* args ) const;
 
-	// TODO: Remove
+	// TODO: Remove all below
 	void DispatchCommand( CVarManagedCmdDispatchInfo info );
-	// TODO: Remove
 	void DispatchStringCVarCallback( CVarManagedVarDispatchInfo<const char*> info );
-	// TODO: Remove
 	void DispatchFloatCVarCallback( CVarManagedVarDispatchInfo<float> info );
-	// TODO: Remove
 	void DispatchBoolCVarCallback( CVarManagedVarDispatchInfo<bool> info );
-	// TODO: Remove
 	void DispatchIntCVarCallback( CVarManagedVarDispatchInfo<int> info );
-
-	void InvokeCallback( Handle callbackHnadle, int argsCount, void* args );
 };
