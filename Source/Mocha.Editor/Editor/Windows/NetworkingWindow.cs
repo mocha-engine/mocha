d46f6d1cdb55c87b1896a338c7a3ea2ea19cd160
Alex <alex@gu3.me>
2023-02-27 14:40:56 +0000

    Hide networking window by default (we're not even using it??)

--- Source/Mocha.Editor/Editor/Windows/NetworkingWindow.cs
+++ Source/Mocha.Editor/Editor/Windows/NetworkingWindow.cs
@@ -3,6 +3,11 @@
 [Title( "Networking" )]
 public class NetworkingWindow : EditorWindow
 {
+	public NetworkingWindow()
+	{
+		isVisible = false;
+	}
+
 	public override void Draw()
 	{
 		if ( ImGuiX.BeginWindow( name: $"Networking", ref isVisible ) )
