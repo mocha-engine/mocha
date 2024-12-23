9151580c89a3bde7e2bd589fdf96f704c35331da
Alex <12881812+xezno@users.noreply.github.com>
2024-12-23 14:40:26 +0000

    Re-enable runtime asset compiler

--- Source/Mocha.Hotload/Main.cs
+++ Source/Mocha.Hotload/Main.cs
@@ -4,6 +4,7 @@ using Mocha.Common.Console;
 using Mocha.Hotload.Projects;
 using Mocha.Hotload.Upgrading;
 using Mocha.Hotload.Util;
+using MochaTool.AssetCompiler;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -125,7 +126,7 @@ public static class Main
 			s_manifest.Resources.Content,
 			"content\\core"
 		);
-		// FileSystem.Mounted.AssetCompiler = new RuntimeAssetCompiler();
+		FileSystem.Mounted.AssetCompiler = new RuntimeAssetCompiler();
 
 		// Create assemblies.
 		if ( isDedicatedServer )
